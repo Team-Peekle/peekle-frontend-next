23426ac3dedaf9e64ed1183c3fce59e6a91e8254
duwlsssss <rladuwls0814@gmail.com>
2025-09-07 23:12:30 +0900

    fix: cn에서 기본 tw merge 사용

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,30 +1,6 @@
 import { type ClassValue, clsx } from 'clsx';
-import { extendTailwindMerge } from 'tailwind-merge';
-
-const ctwMerge = extendTailwindMerge({
-  extend: {
-    theme: {
-      text: [
-        'h2',
-        'h3',
-        'p18-20',
-        'p16-17',
-        'p15-16',
-        'p13-14',
-        'p18',
-        'p17b',
-        'p17m',
-        'p16b',
-        'p16sb',
-        'p16m',
-        'p15b',
-        'p15m',
-        'p14',
-      ],
-    },
-  },
-});
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return ctwMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
 }
