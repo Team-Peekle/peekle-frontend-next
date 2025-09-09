85633988f82dcffbeb904ae37e7016b486f82fc1
duwlsssss <rladuwls0814@gmail.com>
2025-09-09 21:00:56 +0900

    chore: zod stable 버전으로 변

--- src/common/constants/queryKeys.ts
+++ src/common/constants/queryKeys.ts
@@ -1,5 +1,7 @@
 import { createQueryKeyStore } from '@lukemorales/query-key-factory';
 
+import { GetEventsParams } from '@features/events/types/api';
+
 // 도메인(기능)별로 키를 그룹화합니다.
 const queryKeys = createQueryKeyStore({
   auth: {
@@ -7,7 +9,7 @@ const queryKeys = createQueryKeyStore({
   },
   events: {
     testToken: null,
-    list: (params) => [params],
+    list: (params: GetEventsParams) => [params],
     detail: (eventId: string) => [eventId],
   },
 });
