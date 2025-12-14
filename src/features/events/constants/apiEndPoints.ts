c179b4bbef005047bf1a1aa6b03f761557a92efd
duwlsssss <rladuwls0814@gmail.com>
2025-12-14 10:54:14 +0900

    fix: 이벤트 엔드포인트 버전 추가

--- src/features/events/constants/apiEndPoints.ts
+++ src/features/events/constants/apiEndPoints.ts
@@ -1,6 +1,6 @@
 export const EVENTS_API_ENDPOINTS = {
-  TEST_TOKEN: 'auth/test-token', // 테스트용
-  EVENTS: 'events',
-  EVENT_DETAIL: (eventId: string) => `events/${eventId}`,
-  SCRAP: (eventId: string) => `events/${eventId}/scrap`,
+  TEST_TOKEN: 'v1/auth/test-token', // 테스트용
+  EVENTS: 'v1/events',
+  EVENT_DETAIL: (eventId: string) => `v1/events/${eventId}`,
+  SCRAP: (eventId: string) => `v1/events/${eventId}/scrap`,
 } as const;
