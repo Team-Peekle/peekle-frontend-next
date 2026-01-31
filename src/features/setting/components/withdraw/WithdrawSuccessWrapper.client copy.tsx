be11b5a3cb079c91164d9b704f8c8523bf1b5cb7
duwlsssss <rladuwls0814@gmail.com>
2026-01-31 23:10:36 +0900

    fix: 탈퇴확인 변수 사용

--- src/features/setting/components/withdraw/WithdrawSuccessWrapper.client copy.tsx
+++ src/features/setting/components/withdraw/WithdrawSuccessWrapper.client copy.tsx
@@ -17,12 +17,7 @@ const WithdrawSuccessWrapper = () => {
     // 쿼리 파라미터를 제거하여 모달을 닫음
     router.replace(ROUTES.ROOT);
   };
-  return (
-    <>
-      isWithdrawSuccess && (
-      <WithdrawSuccessModal onClose={handleCloseModal} />)
-    </>
-  );
+  return <>{isWithdrawSuccess && <WithdrawSuccessModal onClose={handleCloseModal} />}</>;
 };
 
 export default WithdrawSuccessWrapper;
