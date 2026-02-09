e93ef5b360a6aec46c6c0e2a445998dca82b2cc2
duwlsssss <rladuwls0814@gmail.com>
2026-02-09 11:49:30 +0900

    fix: body 태그에 pretendard 적용

--- src/common/layout/DefaultNavbar.client.tsx
+++ src/common/layout/DefaultNavbar.client.tsx
@@ -1,10 +1,15 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
+import { ROUTES } from '@common/constants/routes';
+
 import PeekleLogo from '@/common/components/svg/PeekleLogo';
 import { useIsScrolled } from '@/common/hooks/useIsScrolled';
 
 export default function DefaultNavbar() {
   const isScrolled = useIsScrolled();
+  const router = useRouter();
 
   return (
     <nav
@@ -12,7 +17,10 @@ export default function DefaultNavbar() {
         isScrolled ? 'border-b border-gray-100' : ''
       }`}
     >
-      <PeekleLogo className="w-82pxr h-22pxr text-gray-900" />
+      <PeekleLogo
+        className="w-82pxr h-22pxr cursor-pointer text-gray-900"
+        onClick={() => router.push(ROUTES.ROOT)}
+      />
     </nav>
   );
 }
